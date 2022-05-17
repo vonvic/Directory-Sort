d9829d90f6bc8976f7dca68b798fd42590b6b8d9
vonvic <40438077+vonvic@users.noreply.github.com>
2022-05-17 17:44:55 -0500

    sorting algorithm complete

--- main.py
+++ main.py
@@ -22,12 +22,31 @@ def request_directory() -> str:
 
     return directory
 
+def get_extension(file: str) -> str:
+    dot_index = len(file) - 1
+    for char in reversed(file):
+        if char == '.':
+            break
+        dot_index -= 1
+    return "unknown" if dot_index < 0 else file[dot_index+1:]
+        
+def create_dir(name: str) -> None:
+    if not os.path.isdir(name):
+        os.mkdir(name)
+
+def move_file(source: str, destination: str):
+    os.rename(source, destination)
 
 def sort(directory: str) -> None:
     print("Sorting...", end='')
     
     for file in os.listdir(directory):
-        print(file)
+        full_path_to_file = directory + file
+        if os.path.isfile(full_path_to_file):
+            extension = get_extension(file)
+            new_location = f'{directory}{extension}/{file}'
+            create_dir(directory + extension)
+            move_file(full_path_to_file, new_location)
 
     print("Done!")
 
