fd49366281b196ff03f6548e6c1a1ccbcb73c20b
vonvic <40438077+vonvic@users.noreply.github.com>
2022-05-17 19:28:00 -0500

    added comments and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# File-Sort
+# Directory Sort
+Sorts the files in a directory, where files are moved into folders titled
+with their extensions. Python is used for fast and easy development, as it is
+a familiar language to the author.
+
+## Requirements
+`python 3.10.0`
+
+## How To Run
+In the command line, run
+```
+python3 .
+```
+in the project directory.
+
+Then, you will be met with the prompt
+```
+Enter which directory to sort (`.` for the current directory or `CTRL+C` to quit):
+```
+
+### Examples of Valid Inputs
+```
+/Users/john/Documents
+/Users/john/Downloads
+~/Downloads
+```
+
+After inputting a valid directory input, then you will be prompted with a `Sorting...`; once, sorting is finished, then `Done!` will be appended.
+```
+Sorting...Done!
+```
+
+### Full example:
+```
+$ python3 .
+Enter which directory to sort (`.` for the current directory or `CTRL+C` to quit): ~/Downloads
+Sorting...Done!
+$
+```
--- __main__.py
+++ __main__.py
@@ -1,7 +1,15 @@
-import os
+# Author: Cayas, Von Vic
+# Date: Tue May 17, 2022
+# Description: Requests the user for a directory and sorts that directory.
+
+import os, sys, signal
 
 def directory_input() -> str:
-    directory = input("Enter which directory to sort (`.` for the current directory or CTRL+D to quit): ")
+    '''Returns directory received by the user. Always returns directory path
+    terminated by the forward slash '/' character. Also, expands the `~` as the
+    home directory.'''
+
+    directory = input("Enter which directory to sort (`.` for the current directory or `CTRL+C` to quit): ")
 
     if directory[0] == '~':
         directory = os.path.expanduser('~') + directory[1:]
@@ -12,6 +20,7 @@ def directory_input() -> str:
     return directory
 
 def request_directory() -> str:
+    '''Returns a valid user directory.'''
     directory = directory_input()
     valid_directory = os.path.isdir(directory)
 
@@ -23,33 +32,36 @@ def request_directory() -> str:
     return directory
 
 def get_extension(file: str) -> str:
+    '''Returns the extension of `file`. If no extension is found, then returns
+    "unknown".'''
     dot_index = len(file) - 1
     for char in reversed(file):
         if char == '.':
             break
         dot_index -= 1
     return "unknown" if dot_index < 0 else file[dot_index+1:]
-        
+
 def create_dir(name: str) -> None:
+    '''Creates a directory if it doesn't already exist'''
     if not os.path.isdir(name):
         os.mkdir(name)
 
-def move_file(source: str, destination: str):
-    os.rename(source, destination)
-
 def sort(directory: str) -> None:
+    '''Iterates through every file immediately in `directory` and moves into a
+    a folder (inside that directory) with the name of that extension.'''
     print("Sorting...", end='')
-    
+
     for file in os.listdir(directory):
         full_path_to_file = directory + file
         if os.path.isfile(full_path_to_file):
             extension = get_extension(file)
             new_location = f'{directory}{extension}/{file}'
             create_dir(directory + extension)
-            move_file(full_path_to_file, new_location)
+            os.rename(full_path_to_file, new_location)
 
     print("Done!")
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, lambda _a, _b: sys.exit())
     directory = request_directory()
     sort(directory)
